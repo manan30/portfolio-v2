5585c4712e8884bf152298020e04001b6b7f97d0
Manan Joshi <mananjoshi1995@gmail.com>
2020-06-23 19:04:07 -0400

    Adding global styles file and removing default content

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,7 +1,4 @@
-import { Link } from 'gatsby';
-import PropTypes from 'prop-types';
 import React from 'react';
-
 import { Header as HeaderContainer } from './styled';
 
 // const Header = ({ siteTitle }) => (
@@ -34,15 +31,7 @@ import { Header as HeaderContainer } from './styled';
 // );
 
 function Header() {
-  return <HeaderContainer></HeaderContainer>;
+  return <HeaderContainer />;
 }
-
-Header.propTypes = {
-  siteTitle: PropTypes.string,
-};
-
-Header.defaultProps = {
-  siteTitle: '',
-};
 
 export default Header;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,13 @@
 import React from 'react';
-import Image from '../components/image';
 import Layout from '../components/layout';
-import SEO from '../components/seo';
+import SEO from '../components/SEO';
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-  </Layout>
-);
+function IndexPage() {
+  return (
+    <Layout>
+      <SEO title="Home" />
+    </Layout>
+  );
+}
 
 export default IndexPage;
