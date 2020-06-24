f2d9ab20dd280ea76c04c635783ed03bc182da64
Manan Joshi <mananjoshi1995@gmail.com>
2020-06-23 20:06:31 -0400

    Adding header and navigation section

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,37 +1,54 @@
-import React from 'react';
-import { Header as HeaderContainer } from './styled';
-
-// const Header = ({ siteTitle }) => (
-//   <header
-//     style={{
-//       background: `rebeccapurple`,
-//       marginBottom: `1.45rem`,
-//     }}
-//   >
-//     <div
-//       style={{
-//         margin: `0 auto`,
-//         maxWidth: 960,
-//         padding: `1.45rem 1.0875rem`,
-//       }}
-//     >
-//       <h1 style={{ margin: 0 }}>
-//         <Link
-//           to="/"
-//           style={{
-//             color: `white`,
-//             textDecoration: `none`,
-//           }}
-//         >
-//           {siteTitle}
-//         </Link>
-//       </h1>
-//     </div>
-//   </header>
-// );
+import React, { useState, useEffect } from 'react';
+import { Link } from 'gatsby';
+import {
+  HeaderContainer,
+  NavigationContainer,
+  NavigationItemContainer,
+  NavItem,
+} from './styled';
 
 function Header() {
-  return <HeaderContainer />;
+  const [scrolled, setScrolled] = useState(false);
+
+  useEffect(() => {
+    if (typeof window === 'undefined') return () => {};
+
+    const scrollHandler = () => {
+      if (window.scrollY > 20) setScrolled(true);
+      else setScrolled(false);
+    };
+
+    window.addEventListener('scroll', scrollHandler);
+
+    return () => window.removeEventListener('scroll', scrollHandler);
+  }, []);
+
+  return (
+    <HeaderContainer shadow={scrolled}>
+      <NavigationContainer>
+        <NavigationItemContainer>
+          <Link to="/experience">
+            <NavItem>Experience</NavItem>
+          </Link>
+          <Link to="/education">
+            <NavItem>Education</NavItem>
+          </Link>
+          <Link to="/experience">
+            <NavItem>Projects</NavItem>
+          </Link>
+          <Link to="/blogs">
+            <NavItem>Blogs</NavItem>
+          </Link>
+          <Link to="/skills">
+            <NavItem>Skills</NavItem>
+          </Link>
+          <Link to="/interests">
+            <NavItem>Interests</NavItem>
+          </Link>
+        </NavigationItemContainer>
+      </NavigationContainer>
+    </HeaderContainer>
+  );
 }
 
 export default Header;
--- src/components/Header/styled.js
+++ src/components/Header/styled.js
@@ -1,13 +1,42 @@
 import styled from 'styled-components';
 
-const Header = styled.header`
-  height: 1.5 rem;
-  width: 100%;
+const HeaderContainer = styled.header`
+  position: fixed;
+
+  height: 2rem;
+  width: calc(100% - 2rem);
+  padding: 1rem;
 
   background-color: #ffffff;
 
-  box-shadow: ${(props) => (props.shadow ? '0 0 0.5rem black' : 'none')};
+  box-shadow: ${(props) => (props.shadow ? '0 0 0.6rem #dce1de' : 'none')};
   transition: all 0.3s ease-in-out;
 `;
 
-export { Header };
+const NavigationContainer = styled.nav`
+  height: 100%;
+`;
+
+const NavigationItemContainer = styled.ul`
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+
+  height: 100%;
+  margin: 0 0 0 40%;
+  padding: 0;
+
+  list-style-type: none;
+`;
+
+const NavItem = styled.li`
+  margin: 0;
+  padding: 0;
+`;
+
+export {
+  HeaderContainer,
+  NavigationContainer,
+  NavigationItemContainer,
+  NavItem,
+};
