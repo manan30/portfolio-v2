9c1e5143cd9e7027a413eaf9ade8fb31994e0cdd
Manan Joshi <mananjoshi1995@gmail.com>
2020-07-01 16:37:47 -0400

    Adding plugin for styled-components to fix styling issue

--- gatsby-config.js
+++ gatsby-config.js
@@ -38,7 +38,8 @@ module.exports = {
     {
       resolve: 'gatsby-plugin-react-svg',
       options: { rule: { include: /images/ } }
-    }
+    },
+    `gatsby-plugin-styled-components`
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
