fe3307fb670db47c13b8e6057852217dc726276e
Manan Joshi <mananjoshi1995@gmail.com>
2020-07-17 15:03:57 -0400

    Showing page content only when it is mounted

--- src/pages/404.js
+++ src/pages/404.js
@@ -25,74 +25,76 @@ const NotFoundPage = () => {
 
   return (
     <Layout page="404">
-      <SEO title="404: Not found" />
-      <MainContainer
-        style={{
-          display: 'flex',
-          alignItems: 'center',
-          width: '100%',
-          marginTop: '0',
-          flexDirection: `${hasMounted && isMobile && 'column'}`
-        }}
-      >
-        <Img
-          fluid={placeholder.childImageSharp.fluid}
-          style={{
-            width: `${hasMounted && isMobile ? '100%' : '50%'}`,
-            flex: '1 0 auto'
-          }}
-        />
-        <div
+      <SEO title="Page Not found" />
+      {hasMounted && (
+        <MainContainer
           style={{
             display: 'flex',
             alignItems: 'center',
-            flexDirection: 'column',
-            marginLeft: `${hasMounted && isMobile ? '0' : '5rem'}`
+            width: '100%',
+            marginTop: '0',
+            flexDirection: `${isMobile && 'column'}`
           }}
         >
-          <PageText
-            fontSize={`${hasMounted && isMobile ? '1.8rem' : '3rem'}`}
-            fontWeight="bolder"
+          <Img
+            fluid={placeholder.childImageSharp.fluid}
             style={{
-              marginBottom: '1rem',
-              color: `${
-                themeState.toggled
-                  ? `var(--color-primary-${themeState.themePreference})`
-                  : 'var(--initial-color-primary)'
-              }`
+              width: `${isMobile ? '100%' : '50%'}`,
+              flex: '1 0 auto'
+            }}
+          />
+          <div
+            style={{
+              display: 'flex',
+              alignItems: 'center',
+              flexDirection: 'column',
+              marginLeft: `${isMobile ? '0' : '5rem'}`
             }}
           >
-            I have some bad news for you
-          </PageText>
-          <PageText
-            style={{
-              color: `${
-                themeState.toggled
-                  ? `var(--color-secondary-${themeState.themePreference})`
-                  : 'var(--initial-color-secondary)'
-              }`
-            }}
-          >
-            The page you might be looking for might be removed or is temporary
-            unavailable.
-          </PageText>
-          <Link to="/">
-            <button
-              type="button"
+            <PageText
+              fontSize={`${isMobile ? '1.8rem' : '3rem'}`}
+              fontWeight="bolder"
               style={{
-                height: '3rem',
-                marginTop: '2rem',
-                padding: '1rem',
-                border: 'none',
-                backgroundColor: '#eb6b5c',
-                color: '#fff9ec'
+                marginBottom: '1rem',
+                color: `${
+                  themeState.toggled
+                    ? `var(--color-primary-${themeState.themePreference})`
+                    : 'var(--initial-color-primary)'
+                }`
               }}
             >
-              <PageText fontWeight="bolder">BACK TO HOMEPAGE</PageText>
-            </button>
-          </Link>
-        </div>
-      </MainContainer>
+              I have some bad news for you
+            </PageText>
+            <PageText
+              style={{
+                color: `${
+                  themeState.toggled
+                    ? `var(--color-secondary-${themeState.themePreference})`
+                    : 'var(--initial-color-secondary)'
+                }`
+              }}
+            >
+              The page you might be looking for might be removed or is temporary
+              unavailable.
+            </PageText>
+            <Link to="/">
+              <button
+                type="button"
+                style={{
+                  height: '3rem',
+                  marginTop: '2rem',
+                  padding: '1rem',
+                  border: 'none',
+                  backgroundColor: '#eb6b5c',
+                  color: '#fff9ec'
+                }}
+              >
+                <PageText fontWeight="bolder">BACK TO HOMEPAGE</PageText>
+              </button>
+            </Link>
+          </div>
+        </MainContainer>
+      )}
     </Layout>
   );
 };
